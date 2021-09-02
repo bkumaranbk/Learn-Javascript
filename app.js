632270c6a6fc795c886eb581d55515f9414944a8
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-02 19:28:41 +0530

    console basics

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hello world..!!");
+
+console.log(12131313);
+
+console.log({ name: "Kumaran" });
+
+console.log(true)
+console.log(false)
+
+console.log(false, 133234442, 'this is string', { name: "Kumaran" });
+
+console.warn("this is warning!")
+
+console.error("this is an error!")
+
