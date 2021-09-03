a4143841b08be741976464d3f9bcc86c6927989e
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-03 20:04:25 +0530

    Comparison operators

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-// Assignment Operators
+// Comparison operators
 
-let name = "Kumaran";
+const a = 21;
+const b = 7;
+const c = 7;
 
-let a = 23
+console.log("a = 21");
+console.log("a = 7");
+console.log("a = 7");
 
-a += name
 
-console.log(a)
+console.log('a > b', a > b);
+console.log('a < b',a < b);
+console.log('a == b',a == b);
+console.log('b == c',b == c);
+
+
+// ------------------------------
+
+
+console.log("------------------------------")
+
+const d= 11;
+const e = "11";
+
+console.log(d == e)
+
+const f = 11;
+const g = "11";
+console.log(f === g)  // === Check data type and value also
+
+const h = "11";
+const i = "12";
+console.log(h === i) // === Check data type and value also
+
+
+console.log("---------------------------")
+
+console.log(false && false) 
+console.log(true && false)
+console.log(true && true)
