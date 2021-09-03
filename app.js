065cbf97f9f37f6f3ba55aa38f255aeb4d044620
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-03 19:48:59 +0530

    Assignment operators cont..

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
-// Arithmetic operators
-
-// let age = 23;
-// age = 20;
-// console.log(age);
-
-// const specfic_name = "Kumaran";
-// // const can be re assign 
-// console.log(specfic_name)
-
-const a = 2;
-const b = 3;
-let c = 0;
-
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a**b);
-console.log(a||b);
-console.log(b%a);
-
-console.log("-----------------------------")
-
 // Assignment Operators
-c += 5;
-// c = c + 1
-c++;
 
-c+=3
-c-=3
-c *= 3;
-c /=2
-c *=3
-c**=2
+let name = "Kumaran";
 
-console.log(c)
+let a = 23
+
+a += name
+
+console.log(a)
