a68a3c59297d932b264c94133d76647e5abf8f8c
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-03 20:18:51 +0530

    Logical operators

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-// Comparison operators
+// Logical operators
 
-const a = 21;
-const b = 7;
-const c = 7;
+const a = 2
+const b = 3
 
-console.log("a = 21");
-console.log("a = 7");
-console.log("a = 7");
+const c = 35
 
+console.log("c > a && c < b", c > a && c < b )
+console.log("c > a || c < b", c > a || c < b )
 
-console.log('a > b', a > b);
-console.log('a < b',a < b);
-console.log('a == b',a == b);
-console.log('b == c',b == c);
+// console.log('a == b', a == b)
+// console.log('a != b', a != b)
+// console.log('')
 
+// console.log('Truth table for AND')
+// console.log('')
 
-// ------------------------------
+// console.log('false && false', false && false)
+// console.log('true && true', true && true)
+// console.log('true && false', true && false)
+// console.log('false && true', false && true)
+// console.log('')
 
+// console.log('Truth table for OR')
+// console.log('')
 
-console.log("------------------------------")
-
-const d= 11;
-const e = "11";
-
-console.log(d == e)
-
-const f = 11;
-const g = "11";
-console.log(f === g)  // === Check data type and value also
-
-const h = "11";
-const i = "12";
-console.log(h === i) // === Check data type and value also
-
-
-console.log("---------------------------")
-
-console.log(false && false) 
-console.log(true && false)
-console.log(true && true)
+// console.log('false || false', false || false)
+// console.log('true || true', true || true)
+// console.log('true || false', true || false)
+// console.log('false || true', false || true)
