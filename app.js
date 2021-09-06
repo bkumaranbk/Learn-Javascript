a789da36f845aab5baa5775620d836806d63656d
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-06 18:22:01 +0530

    Function keyword

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const button = document.querySelector('button');
-const element = document.querySelector('#selectme');
-const body = document.querySelector('body');
+// const rock = document.querySelector("#rock");
+// const paper = document.querySelector("#paper");
+// const scissor = document.querySelector("#scissor");
 
-// console.log(button, element)
+// console.log(rock);
+// console.log(paper);
+// console.log(scissor);
 
-button.addEventListener('click', ()=>{
-    // element.style = "background:dodgerblue";
-    element.classList.toggle('dodgorblue')
-})
+function logName(Name = "Default", age = 23){
+    // console.log("kumaran");
+    console.log(Name + " You are " + age);
+}
 
-button.addEventListener('click', ()=>{
-    // element.style = "background:dodgerblue";
-    body.classList.toggle('dark')
-})
+// Never Misspell the function Keyword
+// A function can have multiple parameters
+// You can set default values for the parameters
+// Any variable you difine inside a function's body is local to that function
+logName()
+
+console.log()
+
+function add(a = 0, b = 0) {
+    return a + b
+}
+
+const result = add()
+
+console.log(result)
