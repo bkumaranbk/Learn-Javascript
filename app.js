714ffed81eb57c4157ded9b7acb5497e53dcf388
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-06 18:45:58 +0530

    ES-6 Funstion

--- app.js
+++ app.js
@@ -6,23 +6,38 @@
 // console.log(paper);
 // console.log(scissor);
 
-function logName(Name = "Default", age = 23){
-    // console.log("kumaran");
-    console.log(Name + " You are " + age);
-}
+// function logName(Name = "Default", age = 23){
+//     // console.log("kumaran");
+//     console.log(Name + " You are " + age);
+// }
+
+// logName()
+
 
 // Never Misspell the function Keyword
 // A function can have multiple parameters
 // You can set default values for the parameters
 // Any variable you difine inside a function's body is local to that function
-logName()
 
-console.log()
 
-function add(a = 0, b = 0) {
+
+
+
+// Function Key Word with variable ________________
+
+// const add = function add(a = 0, b = 0) {
+//     return a + b
+// }
+
+// console.log(add(25,25))
+
+
+
+// Arrow Key Word ________________
+
+const add1 = (a = 0, b = 0) => {
     return a + b
 }
 
-const result = add()
+console.log(add1(25,25))
 
-console.log(result)
