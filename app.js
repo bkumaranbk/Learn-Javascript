0c34dbe458e2b23db85225369945d3e3df2f1cd9
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-06 19:29:09 +0530

    Switch statement

--- app.js
+++ app.js
@@ -1,43 +1,75 @@
-// const rock = document.querySelector("#rock");
-// const paper = document.querySelector("#paper");
-// const scissor = document.querySelector("#scissor");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+const board =  document.querySelector("#board");
 
 // console.log(rock);
 // console.log(paper);
 // console.log(scissor);
 
-// function logName(Name = "Default", age = 23){
-//     // console.log("kumaran");
-//     console.log(Name + " You are " + age);
+//==========================================
+// const handler = (action) => {
+//     console.log("You clicked " + action);
 // }
 
-// logName()
+// rock.addEventListener('click', () => {
+//     handler('rock')
+// });
 
 
-// Never Misspell the function Keyword
-// A function can have multiple parameters
-// You can set default values for the parameters
-// Any variable you difine inside a function's body is local to that function
+// paper.addEventListener('click', () => {
+//     handler('paper')
+// });
+
+
+// scissor.addEventListener('click', () => {
+//     handler('scissor')
+// });
+
+
+// ========================================
+// rock.addEventListener('click',(action) => {
+//     console.log("You clicked " + action);
+// });
 
 
 
+// =================Final=========================
+const handler = (event) => {
+    // console.log(event.target.id);
+    const action = event.target.id
 
+    // if(action === 'rock'){
+    //     // console.log("Rock crushes scissors");
+    //     board.innerHTML = "Rock crushes scissors"
+    // }else if (action === 'paper'){
+    //     // console.log("Paper traps Rock");
+    //     board.innerHTML = "Paper traps Rock"
+    // }else if (action === 'scissor'){
+    //     // console.log("Scissor cuts Paper");
+    //     board.innerHTML = "Scissor cuts Paper"
+    // }else{
+    //     // console.log("Click somthing");
+    //     board.innerHTML = "Click somthing"
+    // }
 
-// Function Key Word with variable ________________
+    switch (action) {
+        case "rock":
+            board.innerHTML = "Rock crushes scissors"
+            break
+        case 'paper':
+            board.innerHTML = "Paper traps Rock"
+            break
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper"
+            break
+        default:
+            board.innerHTML = "Click something"
+            break
+    }
 
-// const add = function add(a = 0, b = 0) {
-//     return a + b
-// }
-
-// console.log(add(25,25))
-
-
-
-// Arrow Key Word ________________
-
-const add1 = (a = 0, b = 0) => {
-    return a + b
 }
 
-console.log(add1(25,25))
-
+rock.addEventListener('click',handler);
+paper.addEventListener('click',handler);
+scissor.addEventListener('click',handler);
