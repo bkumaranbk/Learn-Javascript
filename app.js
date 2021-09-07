a77c3df7bf83554cf87b5ac87571e6d5dca155b6
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-07 18:06:37 +0530

    Array methods

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-// +++++ FOR LOOP ++++++++
-// let i;
+const balls = document.getElementsByClassName('ball');
+const body = document.querySelector('body');
 
-// for (i =1; i<10; i++) {
-//     console.log('iteration ' + i);
+
+const arr = [1,2,3,4,5,6]
+
+// arr.push(6);
+// console.log(arr);
+
+// console.log(arr.length);
+// const added = arr.push(3)
+// const added = arr.unshift(3)
+// const added = arr.includes(3)
+// console.log(added);
+
+// arr.shift()
+// arr.splice(2,1) //remove specific index 
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);;
 // }
 
 
-// +++++ WHILE LOOP ++++++++
-// let a = 1;
-
-// while(a < 10 ){
-//     console.log(a);
-//     a++
-// }
-
+arr.forEach((element, index) => {
+    // console.log('iteration');
+    console.log(element, index);
+});
 
