bbd86001c56d1306352549c165c61566cb5d4cf7
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-07 17:32:55 +0530

    For and While Loops

--- app.js
+++ app.js
@@ -1,75 +1,17 @@
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissor = document.querySelector("#scissor");
-const board =  document.querySelector("#board");
+// +++++ FOR LOOP ++++++++
+// let i;
 
-// console.log(rock);
-// console.log(paper);
-// console.log(scissor);
-
-//==========================================
-// const handler = (action) => {
-//     console.log("You clicked " + action);
+// for (i =1; i<10; i++) {
+//     console.log('iteration ' + i);
 // }
 
-// rock.addEventListener('click', () => {
-//     handler('rock')
-// });
+
+// +++++ WHILE LOOP ++++++++
+// let a = 1;
+
+// while(a < 10 ){
+//     console.log(a);
+//     a++
+// }
 
 
-// paper.addEventListener('click', () => {
-//     handler('paper')
-// });
-
-
-// scissor.addEventListener('click', () => {
-//     handler('scissor')
-// });
-
-
-// ========================================
-// rock.addEventListener('click',(action) => {
-//     console.log("You clicked " + action);
-// });
-
-
-
-// =================Final=========================
-const handler = (event) => {
-    // console.log(event.target.id);
-    const action = event.target.id
-
-    // if(action === 'rock'){
-    //     // console.log("Rock crushes scissors");
-    //     board.innerHTML = "Rock crushes scissors"
-    // }else if (action === 'paper'){
-    //     // console.log("Paper traps Rock");
-    //     board.innerHTML = "Paper traps Rock"
-    // }else if (action === 'scissor'){
-    //     // console.log("Scissor cuts Paper");
-    //     board.innerHTML = "Scissor cuts Paper"
-    // }else{
-    //     // console.log("Click somthing");
-    //     board.innerHTML = "Click somthing"
-    // }
-
-    switch (action) {
-        case "rock":
-            board.innerHTML = "Rock crushes scissors"
-            break
-        case 'paper':
-            board.innerHTML = "Paper traps Rock"
-            break
-        case 'scissor':
-            board.innerHTML = "Scissor cuts paper"
-            break
-        default:
-            board.innerHTML = "Click something"
-            break
-    }
-
-}
-
-rock.addEventListener('click',handler);
-paper.addEventListener('click',handler);
-scissor.addEventListener('click',handler);
