5fa0096462aeb2d5b95a40208018e04c685eeb91
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-09 20:50:59 +0530

    Promises

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-let ranks = [1,2,3,4,5,6,7];
+// JS Promises
+// Promises is a JS Object
+// ------------
+// JS Promises Have 3-Stages
+// Pending
+// Fullfilled / Resolve
+// Rejected
 
-let newMapArr = ranks.map((e) => {
-        if(e%2===0){
-            return e
-        }
-    });
-console.log(newMapArr);
+// Promise
+let concert = true;
+let attendConsert = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    if (concert) {
+      resolve("BOB ATTEND THE CONCERT");
+    } else {
+      reject("BOB FAILED TO ATTEND THE CONCERT");
+    }
+  }, 3000);
+});
 
+console.log(attendConsert);
+console.log(attendConsert);
 
-let newFilterArr = ranks.filter((e) => {
-        if(e%2===0){
-            return e
-        }
-    } );
-console.log(newFilterArr);
+// then and catch is a method
+// attendConsert.then((data) => console.log(data));
+
+// attendConsert.catch((error) => console.log(error);
+
+attendConsert.then((data) => console.log(data)).catch((error) => console.log(error));
