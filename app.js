f83fdf0af22f9544b3d4e2d6b64aeb372ef83c47
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-09 18:54:35 +0530

    Arrow function

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-console.log("Hello world!!");
+// //Noraml function
+// function add(x,y){
+//     return x+y;
+// }
 
-const arr = [1,2,3,4]
-console.log(arr[2]);
+// console.log(add(2,4));
 
-const [one, two, three, four, five] = arr; //ES6 Destructuring
-console.log(three);
-console.log(five);
 
-function getScores(){
-    return [90,100];
+// //Function Variable declaration
+// let add1 = function(x,y){
+//     return x+y;
+// }
+// console.log(add1(12,4));
+
+
+// //Arrow Function with varable declaration
+// let add2 = (x,y) => x + y;
+// console.log(add2(13,4));
+
+// //When use multiple return use { }
+// let add3 = (x,y) => {
+//     console.log(x);
+//     return x + y;
+// }
+// console.log(add3(13,4));
+
+
+
+
+
+const counter = {
+    count: 0,
+    manipulate: function(){
+        return ++(this.count);
+    },
+    // key: this.location.href
+    arrow: (x,y) => {
+            return x + y; 
+    },
+    previous: () => {
+        return --(this.count); //Arrow function doesn't have 'this' key word BUT when use "return this;" indecate window
+    },
+
+
 }
 
-let [x,y,z] = getScores();
-console.log(x);
-console.log(y);
-console.log(z);
-
-const obj = {
-    fname: "Balachandran Kumaran",
-    email: "bkumaran98@gmail.com",
-    age: 23
-}
-
-console.log(obj.name);
-
-
-let {fname, age, email, dum} = obj;
-console.log(age);
-console.log(email);
-console.log(dum);
-
-
-let person = {
-    firstname: "Balachandran Kumaran",
-    lastname: "Kumaran",
-    age1: 23,
-    middlename:"bk"
-}
-
-let {firstname, lastname, age1, middlename = ""} = person;
-console.log(firstname);
-console.log(lastname);
-console.log(age1);
-console.log(middlename);
+console.log(counter.manipulate());
+console.log(counter.arrow(12,12));
+console.log(counter.previous());
+console.log(counter.count);
+console.log(counter);
