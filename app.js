24504acb660b96f53ba922a7eefdbee77ef39d9a
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-09 18:52:24 +0530

    ES6 Destructuring

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-// const balls = document.getElementsByClassName('ball');
-const body = document.querySelector('body');
+console.log("Hello world!!");
 
-const balls = document.querySelectorAll('.ball');
+const arr = [1,2,3,4]
+console.log(arr[2]);
 
-balls.forEach((ball, iteration) => {
-    const value = iteration * 50
-    ball.style = `transform:translateY(${value}px)`
-})
+const [one, two, three, four, five] = arr; //ES6 Destructuring
+console.log(three);
+console.log(five);
+
+function getScores(){
+    return [90,100];
+}
+
+let [x,y,z] = getScores();
+console.log(x);
+console.log(y);
+console.log(z);
+
+const obj = {
+    fname: "Balachandran Kumaran",
+    email: "bkumaran98@gmail.com",
+    age: 23
+}
+
+console.log(obj.name);
 
 
+let {fname, age, email, dum} = obj;
+console.log(age);
+console.log(email);
+console.log(dum);
 
-let age = 23;
-let name = "kumaran"
-console.log('Your age is ' +age);
-console.log(`Your age is ${age}`);
-console.log(`Hi ${name} Your age is ${age}`);
+
+let person = {
+    firstname: "Balachandran Kumaran",
+    lastname: "Kumaran",
+    age1: 23,
+    middlename:"bk"
+}
+
+let {firstname, lastname, age1, middlename = ""} = person;
+console.log(firstname);
+console.log(lastname);
+console.log(age1);
+console.log(middlename);
