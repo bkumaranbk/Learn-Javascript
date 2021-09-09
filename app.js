2b04fe48443d6812d3ece4408185500334723809
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2021-09-09 19:39:55 +0530

    ES6-Array Methods

--- app.js
+++ app.js
@@ -1,51 +1,32 @@
-// //Noraml function
-// function add(x,y){
-//     return x+y;
-// }
+let ranks = [1,2,3];
+// const newArr = ranks.map((e) => console.log(e+1));
 
-// console.log(add(2,4));
-
-
-// //Function Variable declaration
-// let add1 = function(x,y){
-//     return x+y;
-// }
-// console.log(add1(12,4));
-
-
-// //Arrow Function with varable declaration
-// let add2 = (x,y) => x + y;
-// console.log(add2(13,4));
-
-// //When use multiple return use { }
-// let add3 = (x,y) => {
-//     console.log(x);
-//     return x + y;
-// }
-// console.log(add3(13,4));
+// console.log(
+//     ranks.map((e) => (e+1)));
 
 
 
+// console.log(
+//     ranks.map((e) => {
+//         return (e+1)
+//     } ));
+
+// console.log(
+//     ranks.map((e, index) => {
+//         return (index)
+//     } )); //map returns 
+
+// console.log(
+//     ranks.forEach((e) => {
+//         return (e+1)
+//     } )); //forEach Does not return any thing
 
 
-const counter = {
-    count: 0,
-    manipulate: function(){
-        return ++(this.count);
-    },
-    // key: this.location.href
-    arrow: (x,y) => {
-            return x + y; 
-    },
-    previous: () => {
-        return --(this.count); //Arrow function doesn't have 'this' key word BUT when use "return this;" indecate window
-    },
-
-
-}
-
-console.log(counter.manipulate());
-console.log(counter.arrow(12,12));
-console.log(counter.previous());
-console.log(counter.count);
-console.log(counter);
+console.log(
+    ranks.map((e) => {
+        if(e%2===0){
+            return e
+        }else{
+            return "odd"
+        }
+    } ));
